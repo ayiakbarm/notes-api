d9e7725b714debda719246a49aa232791a3ca7c2
Ayi Akbar <icamaulana2612@gmail.com>
2023-12-10 10:21:50 +0700

    adding jwt for authorization

---
+++ migrations/1702171841794_create-table-authentications.js
@@ -0,0 +1,14 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable('authentications', {
+    token: {
+      type: 'text',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('authentications');
+};
